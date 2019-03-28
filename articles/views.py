3624e4288c9db0811e3b383a2056adb9683f709f
Sohail Shaikh <sohailzone@users.noreply.github.com>
2019-03-28 11:16:41 +0530

    commit 28/3

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -11,3 +12,7 @@ def article_detail(request,slug):
     #return HttpResponse(slug)
     article_slug  = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article':article_slug})
+
+#@login_required(login_url="/accounts/login")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
